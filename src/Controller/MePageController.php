bd59c00d19bad5f12db21663e372fcad032dabf1
Patrik Arvius <patrik.arvius@gmail.com>
2024-04-29 17:26:22 +0200

    Added some style, images and routes

---
+++ src/Controller/MePageController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MePageController extends AbstractController
+{
+    #[Route("/", name: "home")]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/lucky", name: "lucky_number")]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'number' => $number
+        ];
+
+        return $this->render('lucky_number.html.twig', $data);
+    }
+}
