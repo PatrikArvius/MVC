8588ee7cfb6ba6a39d1d173cb369a12530540ed5
Patrik Arvius <patrik.arvius@gmail.com>
2024-06-01 10:01:45 +0200

    Add controller and api routes for displaying books in Json format

---
+++ src/Controller/LibraryJsonController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BookRepository;
+use App\Entity\Book;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LibraryJsonController extends AbstractController
+{
+    #[Route('/api/library/books', name: 'app_library_all_books_json')]
+    public function allBooksJson(
+        BookRepository $bookRepository
+    ): Response {
+        $books = $bookRepository->findAll();
+        $jsonBooks = [];
+
+        if ($books) {
+            foreach ($books as $book) {
+                $title = $book->getTitle();
+                $author = $book->getAuthor();
+                $isbn = $book->getIsbn();
+                $image = $book->getImage();
+                $jsonBooks[] = ['title' => $title,
+                            'author' => $author,
+                            'isbn' => $isbn,
+                            'image-url' => $image
+                        ];
+            }
+            $data = [
+                'books' => $jsonBooks
+            ];
+            return new JsonResponse($data);
+        }
+        return $this->redirectToRoute('app_library_reset');
+    }
+
+    #[Route('/api/library/book/{isbn}', name: 'app_library_one_book_json')]
+    public function oneBookJson(
+        int|float $isbn,
+        BookRepository $bookRepository
+    ): Response {
+        $isbn = intval($isbn);
+        $book = $bookRepository
+            ->findOneBy(['isbn' => $isbn]);
+        $data = [
+            'book' => $book
+        ];
+
+        if ($book) {
+            $title = $book->getTitle();
+            $author = $book->getAuthor();
+            $bookIsbn = $book->getIsbn();
+            $image = $book->getImage();
+
+            $data = [
+                'title' => $title,
+                'author' => $author,
+                'isbn' => $bookIsbn,
+                'image-url' => $image
+            ];
+
+            return new JsonResponse($data);
+        }
+        return $this->redirectToRoute('app_library_reset');
+    }
+}
